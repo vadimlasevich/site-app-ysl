5a0ac6813319a3a76a2d48740f6a09da67edf3d3
vadimlasevich <lasevichv@mail.ru>
2024-09-06 09:32:24 +0300

    add animation (page Repair), new texts

---
+++ src/components/AnimatedBackgroundPage.jsx
@@ -0,0 +1,95 @@
+import React, { useCallback } from 'react';
+import { Particles } from 'react-tsparticles';
+import { loadSlim } from 'tsparticles-slim';
+
+const AnimatedBackgroundPage = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <div style={{ position: 'fixed', height: '100vh', width: '100%', zIndex: -1 }}>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: 'transparent',
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 100,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#d5d5d5',
+            },
+            links: {
+              color: '#d8d8d8',
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 2,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 100,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default AnimatedBackgroundPage;
